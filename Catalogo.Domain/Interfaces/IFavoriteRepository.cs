dac9f9d6f4739fb8aff0d92f95230233c2cf7f8b
Dave2826 <david.morales@tecdesoftware.edu.mx>
2026-05-24 18:21:16 -0600

    corrigiendo persistencia y aplicando interfaz de favoritos

---
+++ Catalogo.Domain/Interfaces/IFavoriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using CatalogoApp.Domain.Interfaces;
+
+namespace CatalogoApp.Domain.Interfaces
+{
+    public class JsonFavoriteRepository : IFavoriteRepository
+    {
+        List<int> ObtenerFavoritos(string usuario);
+
+        void GuardarFavoritos(
+            string usuario,
+            List<int> favoritos
+        );
+    }
+}
